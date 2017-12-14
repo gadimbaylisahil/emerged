c32fec88e14fb8824504c916df0ec3e9c4943b3f
Sahil <sahil.gadimbay@gmail.com>
2017-12-14 02:03:21 +0100

    nest profile resources under :user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_devise_parameters, if: :devise_controller?
-
   protected
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :role])
   end
 
-  def after_sign_in_path(resource)
+  def after_sign_in_path_for(resource)
+    byebug
     case resource.role
       when 'artist'
         redirect_to artist_profile_path(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :supporter_profiles
   devise_for :users
-  resources :artist_profiles
+  resources :users do
+    resource :artist_profiles
+    resource :supporter_profiles
+  end
   resources :creations
   resources :stories
   root 'artists#show'
