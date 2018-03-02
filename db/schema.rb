9b3dcb68420c1baae83096334fe7618ef11bae0d
Sahil <sahil.gadimbay@gmail.com>
2018-03-02 11:43:45 +0100

    run pending migrations for Settings

--- db/schema.rb
+++ db/schema.rb
@@ -49,15 +49,6 @@ ActiveRecord::Schema.define(version: 20180301222943) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "stories", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title"
-    t.text "body"
-    t.integer "status"
-    t.text "bg_image"
-  end
-
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
     t.string "taggable_type"
