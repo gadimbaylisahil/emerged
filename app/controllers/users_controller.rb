c8a80987c14d5486c229bf93aebc0f250e775784
Sahil <sahil.gadimbay@gmail.com>
2018-03-03 19:49:25 +0100

    add first_name and last_name to fields in registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < Clearance::UsersController
+  private
+
+  def user_params
+    super
+    params.require(:user).permit(:email, :password, :first_name, :last_name)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   include Clearance::User
+  has_one :profile
+  has_one :setting
+
+  acts_as_tagger
+  acts_as_followable
+  acts_as_follower
 end
