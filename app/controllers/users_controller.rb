df362aa0302df633593dc4db6229b91913afd263
Sahil <sahil.gadimbay@gmail.com>
2018-03-03 21:35:11 +0100

    Remove overriding create method from parent controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,7 @@ class UsersController < Clearance::UsersController
   def new
     @user = User.new
   end
-
-  def create
-    @user = user_from_params
-
-    if @user.save
-      sign_in @user
-      redirect_to root_path
-    else
-      render template: "users/new"
-    end
-  end
-
+  
   private
 
   def user_params
