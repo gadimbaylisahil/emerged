485654c3185d9f27dc5e1933bc70c6410ba9a057
Sahil <sahil.gadimbay@gmail.com>
2018-03-03 21:24:18 +0100

    change login and register views, fix param issues with user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < Clearance::UsersController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_to root_path
+    else
+      render template: "users/new"
+    end
+  end
+
   private
 
   def user_params
-    super
     params.require(:user).permit(:email, :password, :first_name, :last_name)
   end
 end
