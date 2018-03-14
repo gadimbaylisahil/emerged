43c7c5d3f992ba0caf8d07cc3f4f16fea9db9514
Sahil <sahil.gadimbay@gmail.com>
2018-03-14 15:44:50 +0100

    Add custom message for username validation, finalize creation card view on dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@ class User < ApplicationRecord
   acts_as_followable
   acts_as_follower
 
-  validates :username, uniqueness: true, case_sensitivity: false
+  validates :username, uniqueness: true, case_sensitivity: false, message: "%{value} is already taken."
+
+  def full_name
+    [self.first_name, self.last_name].join(" ")
+  end
 end
