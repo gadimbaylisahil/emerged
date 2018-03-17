ebedb8558f518651cdfb8ddd2ce898c32b2dd1aa
Sahil <sahil.gadimbay@gmail.com>
2018-03-17 02:11:04 +0100

    Generate Subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317005709) do
+ActiveRecord::Schema.define(version: 20180317010956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,15 @@ ActiveRecord::Schema.define(version: 20180317005709) do
     t.text "bg_image"
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "chat_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_subscriptions_on_chat_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
+  end
+
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
     t.string "taggable_type"
@@ -150,4 +159,6 @@ ActiveRecord::Schema.define(version: 20180317005709) do
   add_foreign_key "creations", "users"
   add_foreign_key "messages", "users"
   add_foreign_key "settings", "users"
+  add_foreign_key "subscriptions", "chats"
+  add_foreign_key "subscriptions", "users"
 end
