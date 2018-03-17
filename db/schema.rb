bcdb10bafc15c1e20d4133bf9b5f70419d4887d2
Sahil <sahil.gadimbay@gmail.com>
2018-03-17 01:54:49 +0100

    Create Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312200845) do
+ActiveRecord::Schema.define(version: 20180317005412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 20180312200845) do
     t.datetime "updated_at"
     t.index ["followable_id", "followable_type"], name: "fk_followables"
     t.index ["follower_id", "follower_type"], name: "fk_follows"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -134,5 +142,6 @@ ActiveRecord::Schema.define(version: 20180312200845) do
   end
 
   add_foreign_key "creations", "users"
+  add_foreign_key "messages", "users"
   add_foreign_key "settings", "users"
 end
