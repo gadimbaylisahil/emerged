e86c6b2f538812cc29bca646e2b0cb6e56904676
Sahil <sahil.gadimbay@gmail.com>
2018-03-17 01:33:14 +0100

    remove nullify from dependedent actions

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,4 +1,4 @@
 class Categorization < ApplicationRecord
-  belongs_to :category, dependent: :nullify
-  belongs_to :creation, dependent: :nullify
+  belongs_to :category
+  belongs_to :creation
 end
--- app/models/creation.rb
+++ app/models/creation.rb
@@ -1,7 +1,7 @@
 class Creation < ApplicationRecord
   belongs_to :user
 
-  has_many :categorizations, dependent: :nullify
+  has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
   # Alias for acts_as_taggable_on :tags you can pass options instead of default value :skills, :interests etc
   acts_as_taggable
