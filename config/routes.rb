5894af65fa26d736bd0c1ef6cc60839b592c14c2
Sahil <sahil.gadimbay@gmail.com>
2018-04-10 22:15:15 +0200

    add Comment resource and run migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :rewards
   resources :stories do
     member do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_04_214944) do
+ActiveRecord::Schema.define(version: 2018_04_10_200952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 2018_04_04_214944) do
 
   create_table "chats", force: :cascade do |t|
     t.string "identifier"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
