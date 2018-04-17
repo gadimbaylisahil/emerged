a478ea943ff6682e1f9b6952ec93c6acf6e43fcf
Sahil <sahil.gadimbay@gmail.com>
2018-04-17 23:58:10 +0200

    some fixes on creations and stories views

--- app/helpers/creation_helper.rb
+++ app/helpers/creation_helper.rb
@@ -1,5 +1,5 @@
 module CreationHelper
-  def creation_cover_photo_url(creation)
+  def creation_cover_photo_url(creation:)
     return image_path('image_placeholder') unless creation.cover_photo.attached?
     image_path url_for(creation.cover_photo)
   end
