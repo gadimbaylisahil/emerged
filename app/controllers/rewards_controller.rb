69214e7ec76764980ecf04a4dc959d447685b80d
Sahil <sahil.gadimbay@gmail.com>
2018-04-21 22:51:06 +0200

    add fields to rewards

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,57 @@
 class RewardsController < ApplicationController
+  include Notifications
+  layout 'dashboard'
+  before_action :require_login, only: %i[new create edit update destroy]
+  before_action :get_reward, only: %i[show]
+  before_action :find_reward, only: %i[edit update destroy]
+
+  def index
+    current_user.rewards
+  end
+
+  def new
+    @reward = current_user.rewards.new
+  end
+
+  def create
+    @reward = current_user.rewards.new(reward_params)
+    if @reward.save
+      flash[:success] = 'You have created a new Reward.'
+      render_notification(flash[:success], 'success')
+    else
+      flash[:error] = @reward.errors.full_messages.first
+      render_notification(flash[:error], 'danger')
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @reward.update(creation_params)
+      flash[:success] = 'Reward has been updated.'
+      render_notification(flash[:success], 'success')
+    else
+      flash[:error] = @reward.errors.full_messages.first
+      render_notification(flash[:error], 'danger')
+    end
+  end
+
+  def destroy
+    @reward.delete
+    flash[:success] = 'Reward has been deleted.'
+    render_notification(flash[:success], 'success')
+  end
+
+  private
+  def get_reward
+    @reward = Reward.find(params[:reward_id] || params[:id])
+  end
+
+  def reward_params
+    params.require(:reward).permit()
+  end
+
+  def find_reward
+    @reward = current_user.rewards.find(params[:reward_id] || params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_21_201706) do
+ActiveRecord::Schema.define(version: 2018_04_21_203740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,11 @@ ActiveRecord::Schema.define(version: 2018_04_21_201706) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "cover_image"
+    t.string "title"
+    t.text "description"
+    t.string "type"
+    t.boolean "visible?", default: false, null: false
     t.index ["user_id"], name: "index_rewards_on_user_id"
   end
 
