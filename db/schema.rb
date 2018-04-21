a27f669757cb07695b35d96dcdfff92f0054d47b
Sahil <sahil.gadimbay@gmail.com>
2018-04-21 22:19:31 +0200

    add published to creations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_21_200602) do
+ActiveRecord::Schema.define(version: 2018_04_21_201706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2018_04_21_200602) do
     t.boolean "disable_comments", default: false
     t.bigint "user_id"
     t.bigint "number_of_views", default: 0, null: false
+    t.boolean "published?", default: false, null: false
     t.index ["user_id"], name: "index_creations_on_user_id"
   end
 
