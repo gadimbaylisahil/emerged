0e9fdc4e565bf2ad0dd11a249b427e5d7f88db5c
Sahil <sahil.gadimbay@gmail.com>
2018-05-06 03:46:45 +0200

    remove publishable concern, remove validation from activity

--- app/controllers/concerns/trackable.rb
+++ app/controllers/concerns/trackable.rb
@@ -1,8 +1,8 @@
 module Trackable
   extend ActiveSupport::Concern
-  PUBLIC_ACTIVITIES = %i[like publish follow comment update]
+  PUBLIC_ACTIVITIES = %i[like publish follow comment]
   def create_activities(subject:, user:)
-    Activity.create(
+    Activity.create!(
         subject: subject,
         activity_type: action_name,
         user: user,
