395b7d31a6166bc90e77b21cc059ddf9f9d3e8d3
Sahil <sahil.gadimbay@gmail.com>
2018-06-29 22:36:01 +0200

    Include liked_user_ids in Creation serializer

--- app/serializers/creation_serializer.rb
+++ app/serializers/creation_serializer.rb
@@ -9,6 +9,14 @@ class CreationSerializer
     creation.get_likes.count
   end
 
+  attribute :username do |creation|
+    creation.user.username
+  end
+
+  attribute :liker_ids do |creation|
+    creation.votes_for.up.voters.pluck(:id)
+  end
+
   belongs_to :user
   belongs_to :category
   has_many   :comments, as: :commentable
