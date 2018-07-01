44c4741f009e72782416e37fcd804759f082e43d
Sahil <sahil.gadimbay@gmail.com>
2018-07-01 16:47:16 +0200

    fix issue with sending yourself notification in case of likes and follows

--- app/controllers/concerns/notifiable.rb
+++ app/controllers/concerns/notifiable.rb
@@ -2,6 +2,7 @@ module Notifiable
   extend ActiveSupport::Concern
 
   def create_notification(subject:, actor_user:, recipient_user:, activity_type:)
+    return if actor_user == recipient_user
     Notification.create!(
                     actor_user: actor_user,
                     recipient_user: recipient_user,
