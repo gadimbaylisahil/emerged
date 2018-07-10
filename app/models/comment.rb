a9c5e6144f6952ff77d06ae9ac2b5e02f456f940
gadimbaylisahil <sahil.gadimbay@gmail.com>
2018-07-10 16:02:52 +0200

    Cache comment counts and incremenet on create for commentable models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,22 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  
+  # Commentable models should include cached_comments_count int field in their table
   belongs_to :commentable, polymorphic: true
 
   validates :body, presence: true
+  
+  after_create :increment_cache_comments_count
+  after_destroy :decrement_cache_comments_count
+  private
+  
+  def increment_cache_comments_count
+    commentable.cached_comments_count += 1
+    commentable.save!
+  end
+  
+  def decrement_cache_comments_count
+    commentable.cached_comments_count -= 1
+    commentable.save!
+  end
 end
--- app/models/creation.rb
+++ app/models/creation.rb
@@ -19,19 +19,18 @@ class Creation < ApplicationRecord
   scope :most_viewed, ->           { order(impressions_count: :desc) }
   scope :most_recent, ->           { order(created_at: :desc)  }
   scope :most_shared, ->           { order(number_of_shares: :desc) }
+  scope :most_discussed, ->        { order(cached_comments_count: :desc) }
+  # Mapping allows us to convert the returned array of objects into ActiveRecord:Relation in order to not cause issues when
+  # chaining other scopes afterwards
   scope :trending,    ->           { where(id: all.sort_by(&:trending_ratio).map(&:id).reverse)}
   
   # Filter Scopes
   scope :today,       ->           { where('created_at >= ?', Date.today ) }
   scope :this_month,  ->           { where('created_at >= ?', Date.current.at_beginning_of_month) }
   scope :this_year,   ->           { where('created_at >= ?', Date.current.at_beginning_of_year) }
-  
-  
   scope :published,   ->           { where(published: true) }
   scope :subscribed,  -> (user_id) { where(category_id: User.find(user_id).following_by_type('Category').pluck(:id)) }
   scope :by_location, -> (country) { joins(:user).where("lower(users.country) = ?", country.downcase) }
-  # Mapping allows us to convert the returned array of objects into ActiveRecord:Relation in order to not cause issues when
-  # chaining other scopes afterwards
 
   def self.followed_by(user)
     following_ids = user.all_following.pluck(:id)
