bdfd9ccf8a80d6b65af2a5d727fb3d92cd48454b
Sahil <sahil.gadimbay@gmail.com>
2018-07-31 00:52:43 +0200

    Move index action to top LicensesController

--- app/controllers/v1/licenses_controller.rb
+++ app/controllers/v1/licenses_controller.rb
@@ -1,14 +1,14 @@
 module V1
 	class LicensesController < V1::ApplicationController
 		
-		def show
-			license = find_license
-			render json: LicenseSerializer.new(license).serialized_json, status: :ok
-		end
-		
 		def index
 			licenses = License.all
 			render json: LicenseSerializer.new(licenses).serialized_json, status: :ok
+		end
+		
+		def show
+			license = find_license
+			render json: LicenseSerializer.new(license).serialized_json, status: :ok
 		end
 		
 		private
